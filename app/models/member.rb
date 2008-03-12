75f473a4b0b972cc4dc1101f4f3121c1274712e8
mlangenberg <mlangenberg@69e7f9e7-3fbb-da11-9af1-000c29b2e418>
2008-03-12 10:40:07 +0000

    changed references to 2007 to 2008

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,12 +10,12 @@ class Member < ActiveRecord::Base
     end
     
     def in_week(week)
-      self.find(:all, :conditions => ['date BETWEEN ? AND ?', Date.commercial(2007, week, 1), Date.commercial(2007, week, 7) ])
+      self.find(:all, :conditions => ['date BETWEEN ? AND ?', Date.commercial(2008, week, 1), Date.commercial(2008, week, 7) ])
     end
     
     def total_hours_in_week(week)
       total=0
-      arr = self.find(:all, :conditions => ['date BETWEEN ? AND ?', Date.commercial(2007, week, 1), Date.commercial(2007, week, 7) ])
+      arr = self.find(:all, :conditions => ['date BETWEEN ? AND ?', Date.commercial(2008, week, 1), Date.commercial(2008, week, 7) ])
       arr.each {|w| total += w.hours_spent }
       total
     end
