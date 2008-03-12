75aff174a737429c4db3c201069941743f10f297
Matthijs Langenberg <mlangenberg@gmail.com>
2008-03-12 12:37:23 +0100

    capified!

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+set :user, "timetrackr.newminds.nl"
+set :application, "s013725.railsnet.nl"
+
+default_run_options[:pty] = true
+set :deploy_to, "~"
+set :use_sudo, false
+set :group_writable, false
+
+set :repository, "git://github.com/mlangenberg/timetrackr.git"
+set :scm, "git"
+set :deploy_via, :remote_cache
+
+role :web, application
+role :app, application
+role :db, application, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    sudo "mongrel_manager start", :as => "manager"
+  end
+  task :stop, :roles => :app do
+    sudo "mongrel_manager stop", :as => "manager"
+  end
+  task :restart, :roles => :app do
+    sudo "mongrel_manager restart", :as => "manager"
+  end
+
+  task :default do
+    update
+    backgroundrb.restart
+    restart
+  end
+  
+  namespace :backgroundrb do
+    task :start do
+      run "backgroundrb_manager start"
+    end
+    task :stop  do
+      run "backgroundrb_manager stop"
+    end
+
+    task :restart do 
+      stop
+      start
+    end
+  end
+end
