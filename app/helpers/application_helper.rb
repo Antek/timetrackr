c12a6081492a77f2e3ba8e96e8ed2f37845bf874
Matthijs Langenberg <mlangenberg@gmail.com>
2008-04-04 16:33:51 +0200

    added linechart for hours per week per member

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,30 @@ module ApplicationHelper
       %{<img src="#{member.timetracks.pie_chart_url_for_week_with_labels(week, @member.used_labels_for_week(week))}" />}
     end
   end
+  
+  def line_chart_for_group(group)
+    GoogleChart::LineChart.new('600x300', "uren per week per persoon", false) do |lc|
+      weeks = group.timetracks.weeks
+      max_per_members = []
+      
+      group.members.each do |member|
+        line_data = weeks.collect { |week| member.timetracks.total_hours_in_week(week) }
+        lc.data member.fullname, line_data, line_color(member.id)
+        max_per_members << line_data.max
+      end
+            
+      lc.show_legend = true
+      lc.axis :x, :range => [weeks.first, weeks.last]
+      lc.axis :y, :range => [0, max_per_members.max]
+      
+      return %{<img src="#{lc.to_url}" />}
+    end
+  end
+  
+  private
+  def line_color(id = rand(15))
+    colors = []
+    colors = (15), (id % 16), (id*3 % 16)
+    colors.map { |c| c.to_s(16) + c.to_s(16) }.join('')
+  end
 end
